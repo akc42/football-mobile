eb0824b611aa2cf92d673276c546c9171871f201
Alan Chandler <alan@chandlerfamily.org.uk>
2020-05-28 21:38:44 +0100

    Add missing directive to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ export default {
   input: [
     'node_modules/lit-element/lit-element.js',
     'node_modules/lit-html/lit-html.js',
+    'node_modules/lit-html/directives/if-defined.js',
     'node_modules/lit-html/directives/cache.js',
     'node_modules/lit-html/directives/class-map.js',
     'node_modules/lit-html/directives/guard.js',
